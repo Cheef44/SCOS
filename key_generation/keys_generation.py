cd5e7fc7308693d7e08e0a812b9d0a16f0974697
Cheef44 <bananchik2211@mail.ru>
2023-10-22 16:33:17 +0300

    feature: adding synchronous and asynchronous encryption key generation function

---
+++ key_generation/keys_generation.py
@@ -0,0 +1,16 @@
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+
+def async_keys_generation(size=1024):
+    keys = RSA.generate(size)
+    key_private = keys.export_key()
+    key_public = keys.public_key().export_key()
+    with open('logs/private.pem', 'wb') as file_keys_private:
+        file_keys_private.write(key_private)
+    with open('logs/public.pem', 'wb') as file_key_puvlic:
+        file_key_puvlic.write(key_public)
+
+def sync_keys_generation(size=32):
+    keys = get_random_bytes(size)
+    with open('logs/sync_key.pem', 'wb') as sync_key:
+        sync_key.write(keys)
