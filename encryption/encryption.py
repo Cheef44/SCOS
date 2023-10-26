b346c9b575d1984663cbd7ddddcd3cc5f9ef0c4e
Cheef44 <bananchik2211@mail.ru>
2023-10-26 20:43:30 +0300

    feature: adding synchronous and asynchronous encryption function

---
+++ encryption/encryption.py
@@ -0,0 +1,31 @@
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.PublicKey import RSA
+
+def async_encryption(files):
+    try:
+        key_file = open('logs/public.pem', 'rb').read()
+    except FileNotFoundError:
+        return 'Not keys'
+    try:
+        file = open(files, 'rb').read()
+        keys = RSA.import_key(key_file).public_key()
+        encrypt = PKCS1_OAEP.new(keys)
+        encrypt = encrypt.encrypt(file)
+        file_encorypt = open(f'{files}.async_encrypt.txt', 'wb')
+        file_encorypt.write(encrypt)
+    except FileNotFoundError:
+        return 'File not'
+
+def sync_encryption(files):
+    try:
+        key_file = open('logs/sync_key.pem', 'rb').read()
+    except FileNotFoundError:
+        return 'Not keys'
+    try:
+        file = open(files, 'rb').read()
+        encrypt = AES.new(key_file, AES.MODE_EAX)
+        encrypt = encrypt.encrypt(file)
+        file_encorypt = open(f'{files}.sync_encrypt.txt', 'wb')
+        file_encorypt.write(encrypt)
+    except FileNotFoundError:
+        return 'File not'
